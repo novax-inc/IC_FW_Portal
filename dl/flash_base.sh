5ee5f2ba5aaad988e9fb4dce2aab97ceeab215df
sansayin <1818237@qq.com>
2022-01-10 11:24:26 -0800

    add flash script

---
+++ dl/flash_base.sh
@@ -0,0 +1,2 @@
+esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0x9000 reset_nvs.bin  0xe000 ota_data_initial.bin   0x1000 bootloader.bin
+
