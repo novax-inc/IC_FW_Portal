e4582dcf1ef34598720d6a712ab54ec1bd8b3ab1
nikkiqin120 <nikkiqin120@gmail.com>
2022-02-01 11:56:24 -0800

    update manual colspan, not show if empty

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -2,7 +2,7 @@
   <div>
     <div>
       <h2>{{ data.Project }}</h2>
-      <div class="box">
+      <div v-show="data.Project.toLowerCase().includes('intellicross')" class="box">
         <a href="./IntelliCross254.apk"
           ><img width="100" src="../assets/android.png"
         /></a>
@@ -12,71 +12,84 @@
     <div>
       <div class="box">
         <table>
-          <tr>
-            <div colspan="4" align="left">
-              <h3>Firmware</h3>
-            </div>
-          </tr>
-          <tr>
-            <th align="left">Date</th>
-            <th align="left">F/W Ver.</th>
-            <th align="left">Link</th>
-            <th align="left">Description</th>
-          </tr>
-          <tr v-for="(record, index) in data['Firmware']" :key="index">
-            <td align="left">{{ record.date }}</td>
-            <td align="left" style="color: #f49f82">
-              {{ record.id }}
-            </td>
-            <td align="left">
-              <a :href="baseurl + data.Project +'/'+ record.url">Download</a>
-            </td>
-            <td align="left">{{ record.description }}</td>
-          </tr>
-          
-          <tr id="whitecell">
-            <div colspan="4" align="left">
-              <h3>Filesystem</h3>
-            </div>
-          </tr>
-          <tr>
-            <th align="left">Date</th>
-            <th align="left">F/W Ver.</th>
-            <th align="left">Link</th>
-            <th align="left">Description</th>
-          </tr>
-          <tr v-for="(record, index) in data['Filesystem']" :key="index">
-            <td align="left">{{ record.date }}</td>
-            <td align="left" style="color: #f49f82">
-              {{ record.id }}
-            </td>
-            <td align="left">
-              <a :href="baseurl + data.Project +'/'+ record.url" download>Download</a>
-            </td>
-            <td align="left">{{ record.description }}</td>
-          </tr>
-
-          <tr id="whitecell">
-            <div colspan="4" align="left">
-              <h3>Manual</h3>
-            </div>
-          </tr>
-          <tr>
-            <th align="left">Date</th>
-            <th align="left">F/W Ver.</th>
-            <th align="left">Link</th>
-            <th align="left">Description</th>
-          </tr>
-          <tr v-for="(record, index) in data['Manual']" :key="index">
-            <td align="left">{{ record.date }}</td>
-            <td align="left" style="color: #f49f82">
-              {{ record.id }}
-            </td>
-            <td align="left">
-              <a :href="baseurl + data.Project +'/'+ record.url" download>Download</a>
-            </td>
-            <td align="left">{{ record.description }}</td>
-          </tr>
+          <!-- ================================================= -->
+          <div v-show="data.Manual && data.Manual.length">
+            <tr>
+              <div colspan="4" align="left">
+                <h3>Manual</h3>
+              </div>
+            </tr>
+            <tr>
+              <th align="left">Date</th>
+              <th align="left">Link</th>
+              <th colspan="2" align="left">File Name</th>
+            </tr>
+            <tr v-for="(record, index) in data['Manual']" :key="index">
+              <td align="left">{{ record.date }}</td>
+              <td align="left">
+                <a :href="baseurl + data.Project + '/' + record.url" download
+                  >Download</a
+                >
+              </td>
+              <td colspan="2" align="left">{{ record.description? record.description: record.url }}</td>
+            </tr>
+          </div>
+          <!-- ================================================= -->
+          <div v-show="data.Firmware && data.Firmware.length">
+            <tr id="title">
+              <div colspan="4" align="left">
+                <h3>Firmware</h3>
+              </div>
+            </tr>
+            <tr>
+              <th align="left">Date</th>
+              <th align="left">Link</th>
+              <th align="left">F/W Ver.</th>
+              <th align="left">Description</th>
+            </tr>
+            <tr v-for="(record, index) in data['Firmware']" :key="index">
+              <td align="left">{{ record.date }}</td>
+              <td align="left">
+                <a :href="baseurl + data.Project + '/' + record.url"
+                  >Download</a
+                >
+              </td>
+              <td align="left" style="color: #f49f82">
+                {{ record.id }}
+              </td>
+              <td align="left">{{ record.description }}</td>
+            </tr>
+          </div>
+          <!-- ================================================= -->
+          <div
+            v-show="
+              data.Filesystem && data.Filesystem.length
+            "
+          >
+            <tr id="title1">
+              <div colspan="4" align="left">
+                <h3>Filesystem</h3>
+              </div>
+            </tr>
+            <tr>
+              <th align="left">Date</th>
+              <th align="left">Link</th>
+              <th align="left">F/W Ver.</th>
+              <th align="left">Description</th>
+            </tr>
+            <tr v-for="(record, index) in data['Filesystem']" :key="index">
+              <td align="left">{{ record.date }}</td>
+              <td align="left">
+                <a :href="baseurl + data.Project + '/' + record.url" download
+                  >Download</a
+                >
+              </td>
+              <td align="left" style="color: #f49f82">
+                {{ record.id }}
+              </td>
+              <td align="left">{{ record.description }}</td>
+            </tr>
+          </div>
         </table>
       </div>
     </div>
@@ -93,8 +106,7 @@ export default {
       masterjson,
       data: {},
       baseurl:
-      "../../Product_Info/"
-        // "https://raw.githubusercontent.com/xiaobingnic/IC_FW_Portal/main/Product_Info/",
+        "https://raw.githubusercontent.com/xiaobingnic/IC_FW_Portal/main/Product_Info/",
     };
   },
   // watch: {
@@ -108,12 +120,14 @@ export default {
   },
   methods: {
     refresh() {
-      console.log(this.$route.params.id)
+      console.log(this.$route.params.id);
 
       for (const idx in this.masterjson) {
         if (
           // this.$route.params.id.toLowerCase() ==
-          this.masterjson[idx].Project.toLowerCase().includes(this.$route.params.id.toLowerCase())
+          this.masterjson[idx].Project.toLowerCase().includes(
+            this.$route.params.id.toLowerCase()
+          )
         ) {
           this.data = this.masterjson[idx];
           break;
@@ -140,14 +154,15 @@ a {
   color: #42b983;
 }
 .box {
-  width: min(95%, 1000px);
+  width: min(95%, 800px);
   display: inline-flex;
   justify-content: center;
 }
-table{
-  border-spacing: 0;
+table {
+  border-spacing: 0.1em;
 }
-#whitecell{
+#title,
+#title1 {
   background-color: #fff;
 }
 </style>
